21ce6858274375aafa982833158742f1927197a9
Raoul v. R <vanruesc@outlook.de>
2018-05-31 16:07:49 +0200

    Version 0.3.0.

--- build/hermite-data-helper.js
+++ build/hermite-data-helper.js
@@ -1,5 +1,5 @@
 /**
- * hermite-data-helper v0.2.2 build Apr 19 2018
+ * hermite-data-helper v0.3.0 build May 31 2018
  * https://github.com/vanruesc/hermite-data-helper
  * Copyright 2018 Raoul van Rüschen, Zlib
  */
