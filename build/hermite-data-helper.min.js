0d340e870cfda68b25e627abf4aad87575764d52
Raoul v. R <vanruesc@users.noreply.github.com>
2020-03-02 15:06:20 +0100

    Version 1.0.3

--- build/hermite-data-helper.min.js
+++ build/hermite-data-helper.min.js
@@ -1,5 +1,5 @@
 /**
- * hermite-data-helper v1.0.2 build Sat Feb 01 2020
+ * hermite-data-helper v1.0.3 build Mon Mar 02 2020
  * https://github.com/vanruesc/hermite-data-helper
  * Copyright 2020 Raoul van Rüschen
  * @license Zlib
